de8a293f34e1de93c264ef717facf8385090889f
abhinavbh08 <abhinavbhatt.999@gmail.com>
2021-11-26 22:33:58 +0100

    Change config

--- src/config.py
+++ src/config.py
@@ -23,8 +23,8 @@ word_embedding_dim = 300
 vocab_path = "models/vocab.pkl"
 
 # Training config
-batch_size = 64
+batch_size = 128
 num_workers = 4
-learning_rate = 0.0002
+learning_rate = 0.0008
 margin = 0.2
 num_epochs = 30
